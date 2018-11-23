9aacf3add37bd795ee77d353e7df1a7d57caa1fe
ernesto <ernestoballono@gmail.com>
2018-11-23 17:42:08 -0500

    initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tito_\onedrive\escritorio\django_practicas\app1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
